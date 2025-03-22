1f1492d4afe53e0cb18fcc86ddaf18d69dcdd8ff
llanquinfranco <llanquinf@gmail.com>
2025-03-22 16:09:57 -0300

    descubriendo linux y cosas raras

--- TP0/ejercicio1.c
+++ TP0/ejercicio1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola Mundo\n");
+    char a = 'a';
+    int b = 300;
+    long c = 13000000;
+    float d = 9.12;
+    double e = 9.1218;
+
+    printf("%c\n",a);
+    printf("%d\n",b);
+    printf("%li\n",c);
+    printf("%f\n",d);
+    printf("%lf\n",e);
+
     return 0;
 }
---
+++ TP0/ejercicio3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    size_t tamano = sizeof(int);
+    printf("El tamaño de int es: %zu bytes\n", tamano);
+    return 0;
+}
