d1ffe9829790df6dc4a84877989f3f8ee1026fa7
llanquinfranco <llanquinf@gmail.com>
2025-03-27 21:57:25 -0300

    acogota2

--- TP0/ahorcado.c
+++ TP0/ahorcado.c
@@ -1,21 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h> /* para las funciones system y exit */
+#include <string.h>
 
 int main() {
 
-    int c;
+    int c, intentos = 5, aciertos = 0, exito;
+    char palabra[] = "queseyo";
+    int longitud = strlen(palabra);
+    char oculta[longitud + 1];
 
-    /* Decirle al sistema que el modo input es RAW */
-    system("/bin/stty raw");
+    // Inicializar la palabra oculta con guiones
+    for (int i = 0; i < longitud; i++) {
+        oculta[i] = '_';
+    }
+    oculta[longitud] = '\0';
 
-    while (1) {
-        printf("\r                                                          ");
-        printf("\r c = %c  ingrese una letra (0 para salir): ", c);
+    while (intentos > 0) {
+        
+        printf("Ingrese una letra (0 para salir): ");
         c = getchar();
+        while (getchar() != '\n');  // Limpiar el búfer de entrada
 
-        if (c == '0')
+        if (c == '0') {
+            printf("Salió del juego\n");
             break;
+        } else {
+            // Comprobamos
+            exito = 0;
+            int i = 0;
+            while (palabra[i] != '\0') {
+                if (palabra[i] == c) {
+                    oculta[i] = c;
+                    exito = 1;
+                    aciertos++;
+                }
+                i++;
+            }
+        }
+        
+        if (aciertos == longitud) {
+            printf("Ganaste, la palabra es: %s\n", palabra);
+            break;
+        }
+
+        if (exito == 0) {
+            intentos--;  // Se reduce el intento solo si falló
+            printf("Erraste: %s\n", oculta);
+            printf("Te quedan %d vidas\n\n", intentos);
+        } else {
+            printf("Acertaste: %s\n", oculta);
+            printf("Te quedan %d vidas\n\n", intentos);
+        }
+
     }
 
-    system("/bin/stty sane erase ^H");
+    if (intentos == 0) {
+        printf("Perdiste, la palabra era: %s\n", palabra);
+    }
+
+    return 0;
 }
+
+/* Decirle al sistema que el modo input es RAW */
+// system("/bin/stty raw");
+// system("/bin/stty sane erase ^H");
